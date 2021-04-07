0f1471c1e4aa21a1c6621ff7ebb23bafff9109cc
Grimm33 <yandrick.alpha.grima@gmail.com>
2021-04-07 12:24:51 +0200

    amended sql styling

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,12 @@
-insert into category (description) values ('American'), ('Italian'), ('Mexican'), ('Fast Food')
+insert into category (description)
+    values ('American'),
+           ('Italian'),
+           ('Mexican'),
+           ('Fast Food');
 
-insert into unit_of_measure (description) values ('Teaspoon'), ('Tablespoon'), ('Cup'), ('Pinch'), ('Ounce')
+insert into unit_of_measure (description)
+    values ('Teaspoon'),
+           ('Tablespoon'),
+           ('Cup'),
+           ('Pinch'),
+           ('Ounce');
