d65b0dd6e6a90720bb8e1a52be717d1bcbdb688b
Grimm33 <yandrick.alpha.grima@gmail.com>
2021-04-07 12:22:25 +0200

    Populating DB with values
    Lesson 140

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into category (description) values ('American'), ('Italian'), ('Mexican'), ('Fast Food')
+
+insert into unit_of_measure (description) values ('Teaspoon'), ('Tablespoon'), ('Cup'), ('Pinch'), ('Ounce')
