62952c4eed6f06059506e31efdc5a760ab09074e
Grimm33 <yandrick.alpha.grima@gmail.com>
2021-04-07 12:33:15 +0200

    Added query methods to Categories and UOM Repos
    Lesson 141

--- src/main/java/ccbill/training/recipeapp/controllers/IndexController.java
+++ src/main/java/ccbill/training/recipeapp/controllers/IndexController.java
@@ -1,13 +1,34 @@
 package ccbill.training.recipeapp.controllers;
 
+import ccbill.training.recipeapp.domain.Category;
+import ccbill.training.recipeapp.domain.UnitOfMeasure;
+import ccbill.training.recipeapp.repositories.CategoryRepository;
+import ccbill.training.recipeapp.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"index", "", "/"})
     public String getIndexPage(){
+
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Cup");
+
+        System.out.println("Cat Id is: " + categoryOptional.get().getId());
+        System.out.println("UOM Id is: " + unitOfMeasureOptional.get().getId());
+
         return "index";
     }
 }
