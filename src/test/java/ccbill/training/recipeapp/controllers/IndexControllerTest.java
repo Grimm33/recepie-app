d22a050f4d028050740a8a83ea37f5efb030a27a
Grimm33 <yandrick.alpha.grima@gmail.com>
2021-04-12 11:37:57 +0200

    Added Index Controller test
    Lesson 176

---
+++ src/test/java/ccbill/training/recipeapp/controllers/IndexControllerTest.java
@@ -0,0 +1,42 @@
+package ccbill.training.recipeapp.controllers;
+
+import ccbill.training.recipeapp.services.RecipeService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.ui.Model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anySet;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.MockitoAnnotations.openMocks;
+
+class IndexControllerTest {
+
+    @Mock
+    RecipeService recipeService;
+
+    @Mock
+    Model model;
+
+    IndexController indexController;
+
+    @BeforeEach
+    void setUp() {
+        openMocks(this);
+
+        indexController = new IndexController(recipeService);
+    }
+
+    @Test
+    void getIndexPage() {
+
+        String viewName = indexController.getIndexPage(model);
+
+        assertEquals("index", viewName);
+        verify(recipeService, times(1)).getRecipes();
+        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+    }
+}
