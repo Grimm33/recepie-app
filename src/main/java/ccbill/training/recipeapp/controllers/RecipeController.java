2797d641cac570fc80aaf6905f63ff483b07b3f0
Grimm33 <yandrick.alpha.grima@gmail.com>
2021-04-14 16:31:48 +0200

    Added ability to update recipe
    Lesson 200

--- src/main/java/ccbill/training/recipeapp/controllers/RecipeController.java
+++ src/main/java/ccbill/training/recipeapp/controllers/RecipeController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+//@RequestMapping({"/recipe/", "/recipe.html"})
 @Controller
 public class RecipeController {
 
@@ -18,16 +19,23 @@ public class RecipeController {
         this.recipeService = recipeService;
     }
 
-    @RequestMapping("/recipe/show/{id}")
+    @RequestMapping({"/recipe/{id}/show", "/recipe/{id}/show/"})
     public String showById(@PathVariable String id, Model model){
         model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
 
         return "recipe/show";
     }
 
-    @RequestMapping("/recipe/new")
+    @RequestMapping({"/recipe/new", "/recipe/new/"})
     public String newRecipe(Model model){
         model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipeform";
+    }
+
+    @RequestMapping({"/recipe/{id}/update", "/recipe/{id}/update/"})
+    public String updateRecipe(@PathVariable String id, Model model){
+        model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
 
         return "recipe/recipeform";
     }
@@ -37,6 +45,6 @@ public class RecipeController {
     public String saveOrUpdate(@ModelAttribute RecipeCommand recipeCommand){
         RecipeCommand savedCommand = recipeService.saveRecipeCommand(recipeCommand);
 
-        return "redirect:/recipe/show/" + savedCommand.getId();
+        return "redirect:/recipe/" + savedCommand.getId() + "/show";
     }
 }
