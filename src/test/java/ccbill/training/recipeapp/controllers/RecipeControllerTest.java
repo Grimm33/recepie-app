bfc19d65e0b5ca616a546ad6e3747346fa069357
Grimm33 <yandrick.alpha.grima@gmail.com>
2021-04-26 09:06:52 +0200

    Adding Data Validation on Recipe Command
    Lesson 234

--- src/test/java/ccbill/training/recipeapp/controllers/RecipeControllerTest.java
+++ src/test/java/ccbill/training/recipeapp/controllers/RecipeControllerTest.java
@@ -85,25 +85,28 @@ class RecipeControllerTest {
         when(recipeService.saveRecipeCommand(any())).thenReturn(command);
 
         mockMvc.perform(post("/recipe")
-                .contentType(MediaType.APPLICATION_FORM_URLENCODED))
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("id", "")
+                .param("description", "some description")
+                .param("directions", "some directions"))
                 .andExpect(status().is3xxRedirection())
                 .andExpect(view().name("redirect:/recipe/2/show"));
     }
 
-//    @Test
-//    public void postNewRecipeFormValidationFailTest() throws Exception {
-//        RecipeCommand recipeCommand = new RecipeCommand();
-//        recipeCommand.setId(2L);
-//
-//        when(recipeService.saveRecipeCommand(any())).thenReturn(recipeCommand);
-//
-//        mockMvc.perform((post("/recipe")
-//                .contentType(MediaType.APPLICATION_FORM_URLENCODED))
-//                .param("id", ""))
-//                .andExpect(status().isOk())
-//                .andExpect(model().attributeExists("recipe"))
-//                .andExpect(view().name("recipe/recipeform"));
-//    }
+    @Test
+    public void postNewRecipeFormValidationFailTest() throws Exception {
+        RecipeCommand recipeCommand = new RecipeCommand();
+        recipeCommand.setId(2L);
+
+        when(recipeService.saveRecipeCommand(any())).thenReturn(recipeCommand);
+
+        mockMvc.perform(post("/recipe")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("id", ""))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("recipe"))
+                .andExpect(view().name("recipe/recipeform"));
+    }
 
     @Test
     public void getUpdateViewTest() throws Exception{
